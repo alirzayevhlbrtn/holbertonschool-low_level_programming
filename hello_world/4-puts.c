096bb4f26194c6226a40ea9d1ebb5e4989047694
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-12 07:15:42 -0700

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char str1[60];
+
+   strcpy(str1, "Programming is like building a multilingual puzzle\n");
+ 
+   puts(str1);
+    
+   return(0);
+}
