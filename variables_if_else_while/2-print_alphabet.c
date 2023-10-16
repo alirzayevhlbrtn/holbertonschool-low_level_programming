f4aaaf96654f31735609175c413e853173dc847a
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-16 05:42:32 -0700

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* print all aphabet */
+	char n;
+
+	/* your code goes there */
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
