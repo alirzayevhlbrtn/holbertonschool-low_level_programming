e1e264fefeda4a7abc55850ab4d2036b20d3ffed
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-16 06:29:19 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* print all aphabet */
+	int n;
+
+	/* your code goes there */
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
