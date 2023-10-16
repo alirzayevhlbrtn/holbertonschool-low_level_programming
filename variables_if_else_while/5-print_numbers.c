24e3d1e7b3593c8a4a33240e569c423f8115ac02
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-16 06:03:49 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* print all aphabet */
+	int n;
+
+	/* your code goes there */
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");	
+	return (0);
+}
