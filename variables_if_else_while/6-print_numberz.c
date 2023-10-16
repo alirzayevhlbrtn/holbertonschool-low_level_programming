f50d93d906c8d322db5a4edf10f37f0a0ac55bdf
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-16 06:14:31 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/* print all aphabet */
+	int n;
+
+	/* your code goes there */
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
