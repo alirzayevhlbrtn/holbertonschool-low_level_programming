f5ff99cd86ec409b14010d0fcc5008e3cfbf87f6
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-17 05:42:46 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		if (i != 9)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
