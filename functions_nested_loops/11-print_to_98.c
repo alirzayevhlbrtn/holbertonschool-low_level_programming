a5f5e7d0335faca7e498531b557164a671ada793
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-18 02:36:56 -0700

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		else if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("98\n");
+}
