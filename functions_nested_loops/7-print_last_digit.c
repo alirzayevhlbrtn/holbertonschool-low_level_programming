bf5f19d5f1112f3fe7514371d563adc68a672ec5
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-18 00:43:13 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: variable
+ * @h: variable
+ *
+ * Return: last digit of integer n
+ */
+int print_last_digit(int n)
+{
+	int h;
+	h = n % 10;
+	if (h < 0)
+	{
+		h = -1 * h;
+	}
+	_putchar(h + '0');
+	return (h);
+}
