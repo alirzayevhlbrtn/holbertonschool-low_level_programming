4fcc4d2e6f50c250d5b3f20ae395e2b04e913129
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-18 02:12:04 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @a: variable1
+ * @b: variable2
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return(c);
+}
