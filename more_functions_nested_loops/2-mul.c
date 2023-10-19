dadf741da3634f753ea297cfe505ee2b8333a043
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-19 06:21:53 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * @a: variable1
+ * @b: variable2
+ * @c: variable3
+ *
+ * Return: 1 - upper OR 0 - lower
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
