e10204bf7afcd59f0a29612bd2fec0f741af98e3
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-23 08:07:33 -0700

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - Entry point
+ *
+ * @str: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if ( i != n - 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
