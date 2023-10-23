ae0e0ea2fdc1e08975718c8f05fcb6059c158881
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-23 08:12:29 -0700

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,8 @@
 /**
  * puts_half - Entry point
  *
- * @str: 'varaibles'
+ * @a: varaible1
+ * @n: variable2
  *
  * Return: Always 0 (Success)
  */
@@ -16,7 +17,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if ( i != n - 1)
+		if (i != n - 1)
 		{
 			putchar(',');
 			putchar(' ');
