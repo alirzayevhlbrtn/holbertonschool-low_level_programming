b88f54552efc27bf5b6ebc038fad61a388743d23
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-24 05:59:15 -0700

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main"
+#include <string.h>
+
+/**
+ * reset_to_98 - check the code
+ *
+ * @n: variable
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int ld = strlen(dest);
+	int ls = strlen(src);
+	char* h[ls + ld];
+
+	for (i = 0; i < ls; i++)
+	{
+		*(dest + ld) = *(src + i);
+		ld++;
+	}
+	return (dest);
+}
