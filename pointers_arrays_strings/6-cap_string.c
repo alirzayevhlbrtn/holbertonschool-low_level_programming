e565571d2976671da3193135758ccf1861a27e22
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-25 07:38:43 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * string_toupper - check the code
+ *
+ * @s: variable
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if ( i == 0)
+		{
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] = s[i] - 32;
+			}
+			i++;
+		}
+		else
+		{
+			if (s[i - 1] <= 34 || s[i - 1] == '?' || s[i - 1] == '.' || s[i- 1] == ',')
+			{
+				if (s[i] >= 97 && s[i] <= 122)
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+			if (s[i - 1] == '\t' || s[i - 1] == ';' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
+			{
+				if (s[i] >= 97 && s[i] <= 122)
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+			i++;
+		}
+	}
+	return (s);
+}
