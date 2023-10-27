6dfaa69fe2e2bc12f7716bfa5566fc18c185eed9
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2023-10-27 06:34:41 -0700

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	do {
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
+	while (s[i] != '\0' && s[i] != c)
+	{
 		i++;
-	} while (s[i] != '\0');
+	}
+	if (s[i] == c)
+	{
+		return (s +i);
+	}
 	return (NULL);
 }
