9733426bae7fd58feca464685686e1d9435f23f0
alirzayevhlbrtn <146727899+alirzayevhlbrtn@users.noreply.github.com>
2023-10-28 17:16:51 +0400

    7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,57 +1,29 @@
-lude "main.h"
-
+#include "main.h"
 #include <stdio.h>
 
-
-
 /**
+ * print_diagsums - check the code
  *
- *  * print_diagsums - check the code
+ * @a: 'var'
+ * @size: 'var'
  *
- *   *
- *
- *    * @a: 'var'
- *
- *     * @size: 'var'
- *
- *      *
- *
- *       * Return: Always 0.
- *
- *        */
-
+ * Return: Always 0.
+ */
 void print_diagsums(int *a, int size)
-
 {
+	int i;
+	int fd = 0;
+	int sd = 0;
 
-		int i;
+	for (i = 0; i < size; i++)
+	{
+		fd = fd + a[i * size + i];
+	}
 
-			int fd = 0;
+	for (i = 0; i < size; i++)
+	{
+		sd = sd + a[i * size + (size - 1 - i)];
+	}
 
-				int sd = 0;
-
-
-
-					for (i = 0; i < size; i++)
-
-							{
-
-										fd = fd + a[i * size + i];
-
-											}
-
-
-
-						for (i = 0; i < size; i++)
-
-								{
-
-											sd = sd + a[i * size + (size - 1 - i)];
-
-												}
-
-
-
-							printf("%d, %d\n", fd, sd);
-
+	printf("%d, %d\n", fd, sd);
 }
