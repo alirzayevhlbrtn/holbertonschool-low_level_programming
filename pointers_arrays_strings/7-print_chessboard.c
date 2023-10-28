67432ef1974be59e0885e475019e87bf26847d25
alirzayevhlbrtn <146727899+alirzayevhlbrtn@users.noreply.github.com>
2023-10-28 17:16:03 +0400

    6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,47 +1,24 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdio.h>
 
-
-
 /**
+ * print_chessboard - check the code
  *
- *  * print_chessboard - check the code
+ * @a: 'var'
  *
- *   *
- *
- *    * @a: 'var'
- *
- *     *
- *
- *      * Return: Always 0.
- *
- *       */
-
+ * Return: Always 0.
+ */
 void print_chessboard(char (*a)[8])
-
 {
+	int i, j;
 
-		int i, j;
-
-
-
-			for (i = 0; i < 8; i++)
-
-					{
-
-								for (j = 0; j < 8; j++)
-
-											{
-
-															_putchar(a[i][j]);
-
-																		if (j == 7)
-
-																							_putchar('\n');
-
-																				}
-
-									}
-
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
 }
