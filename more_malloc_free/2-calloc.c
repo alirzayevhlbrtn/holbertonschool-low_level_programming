784013a5f7f6ad84ab359ea066eadd83aee14358
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-11-15 04:20:10 -0800

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	s = malloc(size * nmemb);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		s[i] = 0;
 	}
