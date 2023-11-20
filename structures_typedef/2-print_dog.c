7162528a632a23db8becda424b3dc1d42a594540
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-11-20 02:01:27 -0800

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,17 +13,17 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	printf("Name: ");
-	if ((*d).name == NULL)
+	if (d->name == NULL)
 		printf("(nil)\n");
 	else
 		printf("%s\n", (*d).name);
 	printf("Age: ");
-	if ((*d).age == NULL)
+	if (d->age == NULL)
 		printf("(nil)\n");
 	else
 		printf("%s\n", (*d).age);
 	printf("Owner: ");
-	if((*d).owner == NULL)
+	if(d->owner == NULL)
 		printf("(nil)\n");
 	else
 		printf("%s\n", (*d).owner);
