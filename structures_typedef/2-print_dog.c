4cf8add966f4871e760e0f88339c6abcab9d8e11
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-11-20 01:58:20 -0800

    2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - gay
+ * @name: gay
+ * @age: gay
+ * @owner: gay
+ * Description: something gay
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: ");
+	if ((*d).name == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", (*d).name);
+	printf("Age: ");
+	if ((*d).age == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", (*d).age);
+	printf("Owner: ");
+	if((*d).owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", (*d).owner);
+}
