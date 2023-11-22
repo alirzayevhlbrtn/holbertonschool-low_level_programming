8f6f8c9ba142311530070248ddf51029ba0ee4bd
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-11-22 04:50:22 -0800

    4

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "calc.h"
+/**
+ * get_op_func - gay
+ * @s: gay
+ * Return: somegay
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 6)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i], f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - gay
+ * @a: gayint1
+ * @b: gayint2
+ * Return: somegay
+ */
+int opt_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - gay
+ * @a: gayint1
+ * @b: gayint2
+ * Return: somegay
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - gay
+ * @a: gayint1
+ * @b: gayint2
+ * Return: somegay
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - gay
+ * @a: gayint1
+ * @b: gayint2
+ * Return: somegay
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - gay
+ * @a: gayint1
+ * @b: gayint2
+ * Return: somegay
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
