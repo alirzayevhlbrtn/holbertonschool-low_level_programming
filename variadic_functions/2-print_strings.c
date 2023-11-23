c22edf3a7eb190803cfc50427f1faff8951cee5c
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-11-23 06:40:38 -0800

    2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - gay
+ * @separator: gay
+ * @n: gay
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	char *s;
+	unsigned int i;
+
+	va_start(ptr, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		s = va_arg(ptr, char *);
+		if (!s)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		if (separator)
+		{
+			printf("%s", separator);
+		}
+	}
+	s = va_arg(ptr, char *);
+	printf("%s", s);
+	printf("\n");
+}
