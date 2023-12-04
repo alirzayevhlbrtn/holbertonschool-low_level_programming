dc6fdc4ecf1715bac7e7d542a4158d06f74a92ae
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-04 05:40:21 -0800

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - adds node to the linked list.
+ * @head: head of linked list.
+ * @str: string.
+ * Return: pointer to new node of the linked list.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->head = *head;
+	*head = new;
+	return (new);
+}
