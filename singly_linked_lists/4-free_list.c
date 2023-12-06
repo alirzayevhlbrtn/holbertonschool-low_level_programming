4e659ed3502c79ef2cf251b73418b9b4f50d840e
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-06 11:17:48 -0800

    4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - gay
+ * @head: list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
