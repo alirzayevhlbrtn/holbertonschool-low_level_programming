619438f1b825a6862e9be85c1f8f5f80bc05e4b7
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-08 03:53:27 -0800

    2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_dnodeint - gay
+ * @head: 1st node
+ * @n: number
+ * Return: new list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+
+	if (!newnode)
+		return (NULL);
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = *head;
+	if((*head))
+		(*head)->prev = newnode;
+	*head = newnode;
+	return(*head);
+}
