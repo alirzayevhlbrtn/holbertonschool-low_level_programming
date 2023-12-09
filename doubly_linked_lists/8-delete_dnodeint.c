e77e23f4213e9fb564d52711d92567cd3842f35f
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-09 11:40:03 -0800

    8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - gay
+ * @head: 1st node
+ * @index: index of delete
+ * Return: integer
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int i = 0;
+
+	if (dlistint_len(*head) <= index)
+		return (-1);
+	temp = *head;
+	if (index == 0)
+	{
+		if ((*head)->next)
+		{
+			(*head) = (*head)->next;
+			(*head)->prev = NULL;
+		}
+		else
+			(*head) = NULL;
+	}
+	else
+	{
+		while (i < index)
+		{
+			temp = temp->next;
+			i++
+		}
+		if (temp->next)
+		{
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+		}
+		else
+			temp->prev->next = NULL;
+	}
+	free(temp);
+	return (1);
+}
