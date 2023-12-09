a42c3a605a360dfab990a55a89ce1b3eafaf6882
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-09 11:18:17 -0800

    7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - gay
+ * @h: 1st node
+ * @idx: index
+ * @n: value
+ * Return: list
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+	dlistint_t *first_part = *h;
+	unsigned int i = 0;
+
+	if (newnode)
+	{
+		newnode->n = n;
+		if (idx == 0)
+		{
+			if (*h)
+			{
+				newnode->next = *h;
+				(*h)->prev = newnode;
+			}
+			*h = newnode;
+			return (newnode);
+		}
+		while (i < idx - 1 && first_part)
+		{
+			first_part = first_part->next;
+			i++
+		}
+		if (first_part)
+		{
+			if (first_part->next)
+				first_part->next->prev = newnode;
+			newnode->next = first_part->next;
+			newnode->prev = first_part;
+			first_part->next = newnode;
+			return (newnode);
+		}
+	}
+	return (NULL);
+}
