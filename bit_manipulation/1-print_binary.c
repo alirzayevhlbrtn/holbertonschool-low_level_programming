f6f5bc8d98be325d0e974919d0bf3799a1cb05ec
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-10 07:39:29 -0800

    1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_printer - gay
+ * @n: int
+ */
+void binary_printer(unsigned long int n)
+{
+	if(!n)
+		return;
+	binary_printer(n >> 1);
+	_putchar((n & 1) + '0');
+}
+/**
+ * print_binary - gay
+ * @n: int
+ */
+void print_binary(unsigned long int n)
+{
+	if (!n)
+		_putchar('0');
+	binary_printer(n);
+}
