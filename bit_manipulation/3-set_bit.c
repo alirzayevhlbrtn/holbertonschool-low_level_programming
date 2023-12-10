d31bbef7d5973287ca8f85ef9c834568f30119b7
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-10 08:03:46 -0800

    3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * set_bit - gay
+ * @n: int
+ * @index: index
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int temp;
+	unsigned int size sizeof(unsigned int) * 8;
+
+	if (index < size)
+	{
+		a = *n;
+		*n = *n >>index;
+		*n = *n | 1;
+		*n = *n << index;
+		*n = *n | a;
+		return (1);
+	}
+	return (-1);
+}
