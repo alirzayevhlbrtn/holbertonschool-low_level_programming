4a2070834760e19e74a8b5e761353e3b8639919a
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-14 05:17:24 -0800

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		text_content = "";
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	wr = write(file, text_content, strlen(text_content));
+	wr = write(fd, text_content, strlen(text_content));
 	if (file == -1 || wr == -1)
 		return (-1);
 	return (1);
