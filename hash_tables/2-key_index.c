d07f93dfb6c5373ea430233314a132df31ebbfcd
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2023-12-15 04:06:49 -0800

    2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - gay
+ * @key: key
+ * @size: size of table
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int h = hash_djb2(key);
+
+	return (key % size);
+}
