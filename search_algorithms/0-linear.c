ce6cf1bcde7c00cdcdb53eba96a320aac0b6f155
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-19 05:09:09 -0800

    test for 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <search_algos.h>
+/**
+ * linear_search - search given array in linear order
+ * @array: pointer to array
+ * @size: size of array
+ * @value: int for search
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return i;
+			}
+		}
+	}
+	return (-1);
+}
